abdd69ff8b9c20038fc328f30f461d7155c26690
Gitsalov Andrey <gicalov.fortech@gmail.com>
2024-12-11 10:14:56 +0300

    fix: code-style

--- microfrontend/src/App.jsx
+++ microfrontend/src/App.jsx
@@ -1,10 +1,9 @@
-// import "./App.css";
 import ToDoWidget from "./Components/ToDoWidget";
 
 const App = () => (
   <div>
     <p>тут мое приложение</p>
-    {/* <div style={{ maxWidth: "200px", overflow: "hidden" }}> */}
+
     <div>
       <ToDoWidget />
     </div>
